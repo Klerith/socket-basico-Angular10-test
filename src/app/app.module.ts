1546b01e8e667c8e564148839bacb7f7ed0f4482
Fernando Herrera <fernando.herrera85@gmail.com>
2020-09-01 14:24:10 -0400

    Pruebas con A10 y sockets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+ 
+const config: SocketIoConfig = { 
+  url: environment.wsUrl, 
+  options: {} 
+};
+ 
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
